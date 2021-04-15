0f2091ff5c8720493402491b16731ad31b98ebe1
Martin Billinger <mbillingr@users.noreply.github.com>
2021-04-15 20:31:04 +0200

    implement spread-combine in scheme

--- scheme/combinators.scm
+++ scheme/combinators.scm
@@ -1,3 +1,34 @@
+(import (sunny arity)
+        (sunny hash-table))
+
+; -----------------------------------
+
+; utils thas should eventually be moved to another file...
+
+(define-syntax assert
+  (simple-macro (condition)
+    (if condition 'ok
+        (error "assertion failed"))))
+
+(define (list . x) x)
+
+(define (length lst)
+  (if (null? lst)
+      0
+      (+ 1 (length (cdr lst)))))
+
+(define (list-tail lst n)
+  (if (= n 0)
+      lst
+      (list-tail (cdr lst) (- n 1))))
+
+(define (list-head lst n)
+  (if (= n 0)
+      '()
+      (cons (car lst)
+            (list-head (cdr lst) (- n 1)))))
+
+; -----------------------------------
 
 (define (compose f g)
   (define (the-composition . args)
@@ -18,3 +49,31 @@
     (h (apply f args)
        (apply g args)))
   the-combination)
+
+(define (spread-combine h f g)
+  (let ((n (get-arity f))
+        (m (get-arity g)))
+    (let ((t (+ n m)))
+      (define (the-combination . args)
+        (assert (= (length args) t))
+        (h (apply f (list-head args n))
+           (apply g (list-tail args n))))
+      (restrict-arity the-combination t))))
+
+(define (restrict-arity proc nargs)
+  (hash-table-set! arity-table proc nargs)
+  proc)
+
+(define (get-arity proc)
+  (let ((entry (hash-table-ref/default arity-table proc #f)))
+    (if entry
+        entry
+        (let ((a (procedure-arity proc)))
+          (assert (eqv? (procedure-arity-min a)
+                        (procedure-arity-max a)))
+          (procedure-arity-min a)))))
+
+(define arity-table (make-key-weak-eq-hash-table))
+
+
+((spread-combine list (lambda (a b) (list 'foo a b)) (lambda (c d e) (list 'bar c d e))) 1 2 3 4 5)
