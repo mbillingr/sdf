f60ea95fd59d66b1e64f285a42dc539a82fb12dc
Martin Billinger <mbillingr@users.noreply.github.com>
2021-05-21 10:57:52 +0200

    exercise 2.11 parts a,b in python

--- python/test_units.py
+++ python/test_units.py
@@ -1,6 +1,8 @@
 import math
 from unittest.mock import Mock
 
+import pytest
+
 import units
 from combinators import compose
 from units import ConversionRegistry
@@ -66,7 +68,7 @@ def test_unit_specialization():
 
     unit.register_conversion(
         "fahrenheit", "kelvin",
-        unit.chain(fahrenheit_to_celsius, celsius_to_kelvin))
+        unit.mul(fahrenheit_to_celsius, celsius_to_kelvin))
 
     unit.register_conversion(
         "pound / inch ** 2",
@@ -77,7 +79,7 @@ def test_unit_specialization():
         "inch ** 3", "meter ** 3",
         unit.pow(inch_to_meter, 3))
 
-    make_specialized_gas_law_volume = units.specializer(
+    make_specialized_gas_law_volume = unit.specializer(
         gas_law_volume,
         output="meter ** 3",
         pressure="newton / meter ** 2",
@@ -90,8 +92,58 @@ def test_unit_specialization():
         temperature="fahrenheit",
         amount="mole")
 
-    result = sphere_radius(conventional_gas_law_volume(14.7, 68, 1))
+    result = sphere_radius(
+        conventional_gas_law_volume(pressure=14.7, temperature=68, amount=1))
     assert math.isclose(result, 7.04962, abs_tol=1e-5)
+
+
+def test_can_register_conversions_only_accepts_registered_conversion_functions():
+    u = ConversionRegistry()
+
+    celsius_to_kelvin = u.make_conversion(lambda c: c + 273.15,
+                                          lambda k: k - 273.15)
+
+    u.register_conversion("celsius", "kelvin", celsius_to_kelvin)
+
+    with pytest.raises(ValueError, match="unknown conversion"):
+        u.register_conversion("foo", "bar", lambda x: x)
+
+
+def test_multiplicative_unit_conversion_composition():
+    u = ConversionRegistry()
+    seconds_to_minutes = u.make_conversion(lambda s: s / 60, lambda m: m * 60)
+    minutes_to_hours = u.make_conversion(lambda m: m / 60, lambda h: h * 60)
+
+    seconds_to_hours = u.mul(seconds_to_minutes, minutes_to_hours)
+
+    assert seconds_to_hours(3600) == 1
+    assert u.invert(seconds_to_hours)(2) == 7200
+
+
+def test_dividing_unit_conversion_composition():
+    u = ConversionRegistry()
+    seconds_to_minutes = u.make_conversion(lambda s: s / 60, lambda m: m * 60)
+    minutes_to_hours = u.make_conversion(lambda m: m / 60, lambda h: h * 60)
+    seconds_to_hours = u.mul(seconds_to_minutes, minutes_to_hours)
+    meters_to_kilometers = u.make_conversion(lambda m: m / 1000,
+                                             lambda k: k * 1000)
+
+    mps_to_kph = u.div(meters_to_kilometers, seconds_to_hours)
+
+    assert math.isclose(mps_to_kph(1), 3.6)
+    assert math.isclose(u.invert(mps_to_kph)(36), 10)
+
+
+def test_exponential_unit_conversion_composition():
+    u = ConversionRegistry()
+
+    inch_to_meter = u.make_conversion(lambda i: i * 0.0254,
+                                      lambda m: m / 0.0254)
+
+    sqinch_to_sqmeter = u.pow(inch_to_meter, 2)
+
+    assert math.isclose(sqinch_to_sqmeter(1550), 1, rel_tol=1e-5)
+    assert math.isclose(u.invert(sqinch_to_sqmeter)(1), 1550, rel_tol=1e-5)
 
 
 def initialize_unit_registry():
--- python/units.py
+++ python/units.py
@@ -1,9 +1,24 @@
 from typing import Callable
 
+from combinators import compose, identity
+
 
 class ConversionRegistry:
     def __init__(self):
         self.inversions = {}
+        self.converters = {}
+
+    def register_conversion(self, from_unit: str, to_unit: str,
+                            convert: Callable):
+        if convert not in self.inversions:
+            raise ValueError("unknown conversion function")
+        self.converters[(from_unit, to_unit)] = convert
+        self.converters[(to_unit, from_unit)] = self.invert(convert)
+
+    def make_converter(self, from_unit: str, to_unit: str) -> Callable:
+        if from_unit == to_unit:
+            return identity
+        return self.converters[(from_unit, to_unit)]
 
     def make_conversion(self, convert: Callable, invert: Callable) -> Callable:
         self.inversions[convert] = invert
@@ -11,6 +26,40 @@ class ConversionRegistry:
 
     def invert(self, convert: Callable):
         return self.inversions.get(convert)
+
+    def mul(self, *units):
+        return self.make_conversion(compose(*units[::-1]),
+                                    compose(*(self.invert(u) for u in units)))
+
+    def div(self, u1, u2):
+        return self.make_conversion(compose(self.invert(u2), u1),
+                                    compose(self.invert(u1), u2))
+
+    def pow(self, unit, n):
+        return self.make_conversion(compose(*[unit] * n),
+                                    compose(*[self.invert(unit)] * n))
+
+    def specializer(self, procedure, output, **implicit_input_units):
+        implicit_output_unit = output
+
+        def specializer(output, **specific_input_units):
+            specific_output_unit = output
+
+            output_converter = self.make_converter(implicit_output_unit,
+                                                   specific_output_unit)
+
+            input_converters = {k: self.make_converter(specific_input_units[k],
+                                                       implicit_input_units[k])
+                                for k in specific_input_units.keys()}
+
+            def specialized_procedure(**kwargs):
+                output = procedure(**{k: input_converters[k](arg)
+                                      for k, arg in kwargs.items()})
+                return output_converter(output)
+
+            return specialized_procedure
+
+        return specializer
 
 
 _REGISTRY = ConversionRegistry()
