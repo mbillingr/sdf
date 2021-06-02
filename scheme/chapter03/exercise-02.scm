ef4c9c59059a077bf240481a5ff029e7bcf18cc0
Martin Billinger <flkazemakase@gmail.com>
2021-06-02 15:26:08 +0200

    implement vector-scalar product

--- scheme/chapter03/exercise-02.scm
+++ scheme/chapter03/exercise-02.scm
@@ -26,6 +26,15 @@ Use the defined arithmetics by passing them to (install-arithmetic!).
              vecs)
         (error "Vector dimension mismatch:" vecs))))
 
+(define (vector-scalar-operation proc)
+  (lambda (vec s)
+    (vector-map (lambda (x) (proc x s)) vec)))
+
+(define (vector-dot-product identity)
+  (let ((mul (vector-element-wise *)))
+    (lambda (v1 v2)
+      (reduce + identity (vector->list (mul v1 v2))))))
+
 (define (vector-extender base-arithmetic)
   (make-arithmetic 'vector vector? (list base-arithmetic)
     (lambda (name base-constant)
@@ -33,24 +42,35 @@ Use the defined arithmetics by passing them to (install-arithmetic!).
     (let ((base-predicate
             (arithmetic-domain-predicate base-arithmetic)))
       (lambda (operator base-operation)
-        (simple-operation operator
-                          vector?
-                          (case operator
-                            ; alternative: implement vector operations in terms of the base operation
-                            ;              this has the drawback that it does not work on vectors of vectors.
-                            ;((+) (vector-element-wise (lambda args (apply-operation base-operation args))))
-                            ((+) (vector-element-wise +))
-                            ((-) (vector-element-wise -))
-                            ((negate) (vector-element-wise -))
-                            ((*) (lambda (v1 v2)
-                                   (reduce
-                                     +
-                                     (arithmetic-constant 'additive-identity
-                                                          base-arithmetic)
-                                     (vector->list ((vector-element-wise *) v1 v2)))))
-                            ((magnitude) (lambda (v) (sqrt (* v v))))
-                            (else
-                              (lambda args
-                                (error "Operator undefined in Vector" operator)))))))))
+        (case operator
+          ((*) (operation-union *
+                 (make-operation operator
+                                 (all-args (operator-arity operator)
+                                           vector?)
+                                 (vector-dot-product
+                                   (arithmetic-constant 'additive-identity
+                                                        base-arithmetic)))
+                 (make-operation operator
+                                 (list (list vector? base-predicate))
+                                 (let ((mul (vector-scalar-operation *)))
+                                   (lambda (vec s) (mul vec s))))
+                 (make-operation operator
+                                 (list (list base-predicate vector?))
+                                 (let ((mul (vector-scalar-operation *)))
+                                   (lambda (s vec) (mul vec s))))))
+          (else
+            (simple-operation operator
+                              vector?
+                              (case operator
+                                ; alternative: implement vector operations in terms of the base operation
+                                ;              this has the drawback that it does not work on vectors of vectors.
+                                ;((+) (vector-element-wise (lambda args (apply-operation base-operation args))))
+                                ((+) (vector-element-wise +))
+                                ((-) (vector-element-wise -))
+                                ((negate) (vector-element-wise -))
+                                ((magnitude) (lambda (v) (sqrt (* v v))))
+                                (else
+                                  (lambda args
+                                    (error "Operator undefined in Vector" operator)))))))))))
 
 (install-arithmetic! (extend-arithmetic vector-extender combined-arithmetic))
