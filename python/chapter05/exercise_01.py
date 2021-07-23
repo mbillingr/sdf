c81ea803d569f13069d4d5229138ab83f511d64a
Martin Billinger <flkazemakase@gmail.com>
2021-07-23 21:53:06 +0200

    @black

--- python/chapter05/exercise_01.py
+++ python/chapter05/exercise_01.py
@@ -1,21 +1,21 @@
+from chapter03.generic_procedures import (
+    any_arg,
+    define_generic_procedure_handler,
+    is_any,
+    simple_generic_procedure,
+)
+
 from generic_interpreter import (
+    INITIAL_ENV_BINDINGS,
     g,
     init,
     is_environment,
+    is_pair,
+    is_symbol,
     is_variable,
     lookup_variable_value,
     match_args,
-    INITIAL_ENV_BINDINGS,
     symbol,
-    is_symbol,
-    is_pair,
-)
-from chapter03.generic_procedures import (
-    simple_generic_procedure,
-    define_generic_procedure_handler,
-    match_args,
-    any_arg,
-    is_any,
 )
 
 
@@ -36,13 +36,23 @@ INITIAL_ENV_BINDINGS[symbol("-")] = sub
 INITIAL_ENV_BINDINGS[symbol("*")] = mul
 INITIAL_ENV_BINDINGS[symbol("/")] = div
 
+
 def is_symbolic(obj):
     return is_symbol(obj) or is_pair(obj)
 
-define_generic_procedure_handler(add, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol('+'), a, b))
-define_generic_procedure_handler(sub, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol('-'), a, b))
-define_generic_procedure_handler(mul, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol('*'), a, b))
-define_generic_procedure_handler(div, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol('/'), a, b))
+
+define_generic_procedure_handler(
+    add, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol("+"), a, b)
+)
+define_generic_procedure_handler(
+    sub, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol("-"), a, b)
+)
+define_generic_procedure_handler(
+    mul, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol("*"), a, b)
+)
+define_generic_procedure_handler(
+    div, any_arg(2, is_symbolic, is_any), lambda a, b: (symbol("/"), a, b)
+)
 
 if __name__ == "__main__":
     init()
