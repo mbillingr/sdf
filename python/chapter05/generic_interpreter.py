695169a48b282ac5b8943c705e9e260fb65b6b48
Martin Billinger <flkazemakase@gmail.com>
2021-07-23 21:49:50 +0200

    add lisp-style list printing

--- python/chapter05/generic_interpreter.py
+++ python/chapter05/generic_interpreter.py
@@ -749,7 +749,8 @@ def repl():
     print("> ", end="")
     for input in g.read():
         ans = g.eval(input, THE_GLOBAL_ENVIRONMENT)
-    print(ans)
+    display(ans)
+    print()
     repl.tailcall()
 
 
@@ -785,6 +786,29 @@ INITIAL_ENV_BINDINGS = {
     Symbol("<="): lambda a, b: a <= b,
     Symbol(">="): lambda a, b: a >= b,
 }
+
+##
+
+display = simple_generic_procedure("display", 1, lambda obj: print(obj, end=""))
+
+
+def display_pair(p, parens=True):
+    if parens:
+        print("(", end="")
+    display(car(p))
+    if is_null(cdr(p)):
+        pass
+    elif is_pair(cdr(p)):
+        print(" ", end="")
+        display_pair(cdr(p), parens=False)
+    else:
+        print(" . ", end="")
+        display(cdr(p))
+    if parens:
+        print(")", end="")
+
+
+define_generic_procedure_handler(display, match_args(is_pair), display_pair)
 
 ##
 
