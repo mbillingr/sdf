e0a7e4368a4c32af55b8367c65e721e0da83677c
Martin Billinger <flkazemakase@gmail.com>
2021-08-16 22:18:07 +0200

    add constraints wording

--- python/chapter05/exercise-5-17.py
+++ python/chapter05/exercise-5-17.py
@@ -1,3 +1,18 @@
+"""
+## Constraints
+- Ben is opposite Eva
+- The man at Alyssa's right has a stronger hand than Lem has.
+- The man at Eva's right has a stronger hand than Ben has.
+- The man at Ben's right has a stronger hand than Cy has.
+- The man at Ben's right has a stronger hand than Eva has.
+- The woman at Lem's right has a stronger hand than Cy has.
+- The woman at Cy's right has a stronger hand than Luis has.
+
+## Variant
+- The man at Ben's right has a stronger hand than Cy has.
+becomes
+- The man on Ben's right is not Cy.
+"""
 from cps_interpreter import repl, eval_str
 
 if __name__ == '__main__':
